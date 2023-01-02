04d45e547d7dbec9a2466173cfc1fe9d04f61274
edsonllima <focusti@hotmail.com>
2023-01-02 11:38:38 -0300

    change App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,19 @@
+import { EditIcon, DeleteIcon } from "@chakra-ui/icons";
+import {
+  Box,
+  Flex,
+  Button,
+  useDisclosure,
+  Table,
+  Thead,
+  Tr,
+  Th,
+  Tbody,
+  Td,
+  useBreakpointValue,
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import ModalComp from "./components/ModalComp";
 const App = () => {
   return <h1>Óla mundo</h1>;
 };
