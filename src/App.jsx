d4e58ab8fc3076e541f618e454a699179c3034bd
edsonllima <focusti@hotmail.com>
2023-01-02 10:22:04 -0300

    change src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,7 @@
-import { useState } from "react";
-import agendaLogo from "../public/logo2.png";
-import "./App.css";
-
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <div>
-        <a href="#" target="_blank">
-          <img src={agendaLogo} className="logo agenda" alt="Agenda logo" />
-        </a>
-      </div>
-      <h1>App de Agendamento</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-    </div>
-  );
+  return;
 }
 
 export default App;
