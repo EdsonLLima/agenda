af72ec0522b8e5e2238815121f27ad8c3650a5a5
edsonllima <focusti@hotmail.com>
2023-01-02 12:55:56 -0300

    change ChakraBaseProvider for ChakraProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import { ChakraBaseProvider } from "@chakra-ui/react";
+import { ChakraProvider } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <ChakraBaseProvider>
+    <ChakraProvider>
       <App />
-    </ChakraBaseProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
