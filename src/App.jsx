7fb4e781dddcf10f6037d1d8e0a76eaf8c015bc4
edsonllima <focusti@hotmail.com>
2023-01-04 01:00:23 -0300

    create pages Home Users Signin

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,8 @@ import {
 } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import ModalComp from "./components/ModalComp";
+import Signin from "./page/Signin";
+import Users from "./page/Users";
 
 const App = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
@@ -41,72 +43,76 @@ const App = () => {
   };
 
   return (
-    <Flex
-      h="100vh"
-      align="cnter"
-      justify="center"
-      fontSize="20px"
-      fontFamily="poppins"
-    >
-      <Box maxW={800} w="100%" h="100vh" py={10} px={2}>
-        <Button
-          textTransform="uppercase"
-          colorScheme="blue"
-          onClick={() => [setDataEdit({}), onOpen()]}
-        >
-          Novo Cadastro
-        </Button>
-        <Box overflowY="auto" height="100%">
-          <Table mt="6">
-            <Thead>
-              <Tr>
-                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
-                  Nome
-                </Th>
-                <Th maxW={isMobile ? 5 : 100} fontSize="20px">
-                  E-mail
-                </Th>
-              </Tr>
-              <Th p={0}></Th>
-              <Th p={0}></Th>
-            </Thead>
-            <Tbody>
-              {data.map(({ name, email }, index) => (
-                <Tr key={index} cursor="pointer" _hover={{ bg: "gray.100" }}>
-                  <Td maxW={isMobile ? 5 : 100}>{name}</Td>
-                  <Td maxW={isMobile ? 5 : 100}>{email}</Td>
-                  <Td p={0}>
-                    <EditIcon
-                      fontSize={20}
-                      onClick={() => [
-                        setDataEdit({ name, email, index }),
-                        onOpen(),
-                      ]}
-                    />
-                  </Td>
-                  <Td p={0}>
-                    <DeleteIcon
-                      fontSize={20}
-                      onClick={() => handleRemove(email)}
-                    />
-                  </Td>
+    <>
+      <Signin />
+      <Users />
+      <Flex
+        h="100vh"
+        align="cnter"
+        justify="center"
+        fontSize="20px"
+        fontFamily="poppins"
+      >
+        <Box maxW={800} w="100%" h="100vh" py={10} px={2}>
+          <Button
+            textTransform="uppercase"
+            colorScheme="blue"
+            onClick={() => [setDataEdit({}), onOpen()]}
+          >
+            Novo Cadastro
+          </Button>
+          <Box overflowY="auto" height="100%">
+            <Table mt="6">
+              <Thead>
+                <Tr>
+                  <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                    Nome
+                  </Th>
+                  <Th maxW={isMobile ? 5 : 100} fontSize="20px">
+                    E-mail
+                  </Th>
                 </Tr>
-              ))}
-            </Tbody>
-          </Table>
+                <Th p={0}></Th>
+                <Th p={0}></Th>
+              </Thead>
+              <Tbody>
+                {data.map(({ name, email }, index) => (
+                  <Tr key={index} cursor="pointer" _hover={{ bg: "gray.100" }}>
+                    <Td maxW={isMobile ? 5 : 100}>{name}</Td>
+                    <Td maxW={isMobile ? 5 : 100}>{email}</Td>
+                    <Td p={0}>
+                      <EditIcon
+                        fontSize={20}
+                        onClick={() => [
+                          setDataEdit({ name, email, index }),
+                          onOpen(),
+                        ]}
+                      />
+                    </Td>
+                    <Td p={0}>
+                      <DeleteIcon
+                        fontSize={20}
+                        onClick={() => handleRemove(email)}
+                      />
+                    </Td>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </Box>
         </Box>
-      </Box>
-      {isOpen && (
-        <ModalComp
-          isOpen={isOpen}
-          onClose={onClose}
-          data={data}
-          setData={setData}
-          dataEdit={dataEdit}
-          setDataEdit={setDataEdit}
-        />
-      )}
-    </Flex>
+        {isOpen && (
+          <ModalComp
+            isOpen={isOpen}
+            onClose={onClose}
+            data={data}
+            setData={setData}
+            dataEdit={dataEdit}
+            setDataEdit={setDataEdit}
+          />
+        )}
+      </Flex>
+    </>
   );
 };
 
