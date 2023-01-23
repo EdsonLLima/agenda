e04426615efc21d1c6dbaf5b38c41f2ffee57f1c
Edson L Lima <focusti@hotmail.com>
2023-01-23 09:16:14 -0300

    add config axios and pull

---
+++ src/axios/config.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const agendaFetch = axios.create({
+  baseURL: "https://my-json-server.typicode.com/EdsonLLima/fakeapi-test",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export default agendaFetch;
